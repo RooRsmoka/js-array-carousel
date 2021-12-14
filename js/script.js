aace3bb54251ba57783924d9eeaa8c1443c3a27f
RooRsmoka <chri.disa96@gmail.com>
2021-12-14 16:55:49 +0100

    commenti sviluppo programma

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Creo array di titoli.
+// Creo array di paragrafi.
+// Creo la variabile currentIndex.
+// Salvo in una variabile l'elemento titolo.
+// Salvo in una variabile l'elemento paragrafo.
+// Salvo in una variabile il "bottone" che userò per cambiare slide (avanti).
+// Salvo in una variabile il "bottone" che userò per cambiare slide (indietro).
+// con la variabile currentIndex a 0 parto col mostrare il titolo e parafrafo con indice 0.
+// Metto in ascolto il "bottone" (avanti) di un evento (click) e
+// incremento il currentIndex per cambiare titolo e paragrafo.
+// Metto in ascolto il "bottone" (indietro) di un evento (click) e
+// decremento il currentIndex per cambiare titolo e paragrafo.
